77bf12687e8a1e03d830928e5f701751c02f15ea
andreas <andreas.oktafiann@gmail.com>
2022-01-27 13:31:20 +0700

    TUGAS JS 11

---
+++ tugas11.js
@@ -0,0 +1,13 @@
+function aritmatika() {
+  var a = 2;
+  var b = 4;
+  var c = 5;
+  var d = 10;
+
+  // console.log((b * d) / a + c);
+  var x = (b * d) / a + c;
+  console.log(x);
+  return;
+}
+
+aritmatika();
